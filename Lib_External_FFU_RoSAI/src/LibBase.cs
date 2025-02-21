18bc5bd3cab4ec9bdb404025a6bc1a18199e2f9d
WarStalkeR <stalker6000@hotmail.com>
2025-02-21 10:36:00 +0200

    External dependencies loader for mods.

---
+++ Lib_External_FFU_RoSAI/src/LibBase.cs
@@ -0,0 +1,38 @@
+﻿using Arcen.Universal;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Arcen.HotM.FFU.RoSAI {
+    public partial class LibInit : IArcenExternalDllInitialLoadCall {
+        public void RunOnFirstTimeExternalAssemblyLoaded() {
+            LoadDependency("FFU_RoSAI", "0Harmony");
+        }
+        public static void LoadDependency(string refModName, string refDllName) {
+            Assembly[] refAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            bool isAlreadyLoaded = refAssemblies.Any(a => string.Equals(a.GetName().Name, refDllName, StringComparison.OrdinalIgnoreCase));
+            if (isAlreadyLoaded) {
+                ArcenDebugging.LogSingleLine($"[LoadDependency] Already Loaded: {refDllName}", Verbosity.DoNotShow);
+                return;
+            }
+            string pModFolder = XmlModTable.GetModByNameOrAltName(refModName).FullDirectoryPath;
+            if (string.IsNullOrEmpty(pModFolder)) {
+                ArcenDebugging.LogSingleLine($"[LoadDependency] Error! Invalid name or inexistent path for '{refModName}' mod.", Verbosity.ShowAsError);
+                return;
+            }
+            string pDllFolder = Path.Combine(pModFolder, "ModdableLogicDLLs");
+            string pDllFile = Path.Combine(pDllFolder, refDllName) + ".dll";
+            if (!File.Exists(pDllFile)) {
+                ArcenDebugging.LogSingleLine($"[LoadDependency] Error! File '{refDllName}' is not found in '{pDllFolder}' folder.", Verbosity.ShowAsError);
+                return;
+            }
+            Assembly aLoadState = Assembly.LoadFrom(pDllFile);
+            if (aLoadState == null) {
+                ArcenDebugging.LogSingleLine($"[LoadDependency] Error! Couldn't load '{pDllFile}' dynamic library.", Verbosity.ShowAsError);
+                return;
+            }
+            ArcenDebugging.LogSingleLine($"[LoadDependency] Successfully Loaded: {aLoadState.GetName().Name}", Verbosity.DoNotShow);
+        }
+    }
+}
